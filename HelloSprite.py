f8c4980b65140e5c4f7bfdbecee8d579d10a6f5a
Zachary Ranes <zachary.ranes@outlook.com>
2022-02-03 20:18:44 -0600

    init

---
+++ HelloSprite.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((700, 700), 0, 32)
+sprite1 = pygame.image.load('images/butterfly.png')
+print()
+
+pygame.display.set_caption("Hello Sprite")
+screen.fill((0, 0, 0))
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    screen.blit(sprite1, (350 - sprite1.get_height() / 2,
+                          350 - sprite1.get_width() / 2))
+    pygame.display.update()
+pygame.quit()
