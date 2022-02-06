ca90a94c523700051d98267f230b760c2112d47d
Zachary Ranes <zachary.ranes@outlook.com>
2022-02-06 10:25:12 -0600

    folder name update

--- 1_Breaking-Bricks/BreakingBricks.py
+++ 1_Breaking-Bricks/BreakingBricks.py
@@ -5,15 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Breakin' Bricks")
 
-bat = pygame.image.load('sprites/paddle.png')
+bat = pygame.image.load('./sprites/paddle.png')
 bat = bat.convert_alpha()
 bat_rec = bat.get_rect()
 
-ball = pygame.image.load('sprites/football.png')
+ball = pygame.image.load('./sprites/football.png')
 ball = ball.convert_alpha()
 ball_rec = ball.get_rect()
 
-brick = pygame.image.load('sprites/brick.png')
+brick = pygame.image.load('./sprites/brick.png')
 brick = brick.convert_alpha()
 brick_rec = brick.get_rect()
 
